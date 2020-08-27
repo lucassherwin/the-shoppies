cec62a8b3f1eb5c50be6f96986e5eee1b6da0c4a
lucassherwin <lksherwin@gmail.com>
2020-08-27 12:43:28 -0400

    added movies to nominations list

---
+++ src/components/Nominations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nominations() {
+    return (
+        <div>
+            <h1>Nominations</h1>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
