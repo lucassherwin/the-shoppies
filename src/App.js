e6546f497ecdc22b60eea09d77639f5e00045628
lucassherwin <lksherwin@gmail.com>
2020-09-07 13:01:25 -0400

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import Search from './components/Search.js';
-import MovieShowPage from './components/MovieShowPage.js';
-// import Nominations from './Nominations.js';
 import axios from 'axios';
 import Nominations from './components/Nominations.js';
 
 export class App extends Component {
     state = {
-        // currentMovie: {
-        //     searchTerm: '',
-        //     title: '',
-        //     releaseYear: null,
-        //     plot: null,
-        //     imdbID: null
-        // },
         searchTerm: '',
         nominations: [], //max of 5
         searchResults: [],
@@ -21,37 +12,8 @@ export class App extends Component {
     }
     
     handleSearch = (searchTerm) => {
-        // console.log('searching');
-        // console.log(searchInput.target.value);
-    
-        // let searchTerm = searchInput.toLowerCase().split(' ').join('+');
-        // this.setState({searchTerm: searchInput.toLowerCase().split(' ').join('+')});
-        // this.setState({searchTerm: searchTerm.target.value})
-        // console.log(this.state.serachTerm);
-        // let search = searchTerm.target.value;
-        // console.log(search);
-    
-        //axios request
-        // axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&t=${search}`)
-        // .then(res => {
-            // this.setState({currentMovie: {...this.state.currentMovie, title: res.data['Title']}})
-            // this.setState({currentMovie: {...this.state.currentMovie, releaseYear: res.data['Year']}})
-            // this.setState({currentMovie: {...this.state.currentMovie, plot: res.data['Plot']}})
-            // this.setState({currentMovie: {...this.state.currentMovie, imdbID: res.data['imdbID']}})
-            // this.state.searchResults.push(res.data) //push the whole movie obj that is returned into the array
-        //     if(res.data['Response'] !== "False")
-        //     {
-        //         console.log('adding: ');
-        //         this.state.searchResults.push(res.data) //push the whole movie obj that is returned into the array
-        //     }
-    
-        //     console.log(res.data)
-        // })
-
         let search = searchTerm.target.value;
         this.setState({searchTerm: search});
-        // this.props.handleSearch(searchTerm);
-        // console.log(search);
         let searchResults = this.state.searchResults;
         let resultString; //this will have the format 'Title (release date)'
     
@@ -71,9 +33,6 @@ export class App extends Component {
     }
 
     nominate = (movieTitle) => {
-		// let currentTitle = this.state.currentMovie.title;
-        // console.log(movieTitle.target.previousSibling.wholeText); //title to be push into nominations
-        console.log(movieTitle.target)
         let movie = movieTitle.target.previousSibling.wholeText; //gets movie string from list item
         let nominations = this.state.nominations;
         if(this.state.nominations.length === 4) //next nomination will fill the array
@@ -93,28 +52,6 @@ export class App extends Component {
             localStorage['nominations'] = JSON.stringify(this.state.nominations); //set localstorage array to nominations from state
             this.setState({searchResults: []}); //reset search results after nominating a movie
         }
-        // if(this.state.nominations.length === 5)
-        // {
-        //     alert('You have selected 5 nominations!');
-        //     console.log(this.state.nominations);
-
-        //   //check if this will make it 5 nominations and alert
-        // }
-        // else
-        // {
-        //     this.state.nominations.push(currentTitle); //only save the title not the whole object
-		// 	//this will make disabling the button easier -> dont need the object at any point
-
-		// 	localStorage['nominations'] = JSON.stringify(this.state.nominations); //set localstorage array to nominations from state
-			
-		// 	console.log(this.state.nominations);
-
-        //     //reset current movie in state
-        //     // this.setState({currentMovie: {...this.state.currentMovie, title: ''}})
-        //     // this.setState({currentMovie: {...this.state.currentMovie, releaseYear: null}})
-        //     // this.setState({currentMovie: {...this.state.currentMovie, plot: ''}})
-        //     // this.setState({currentMovie: {...this.state.currentMovie, imdbID: null}})
-        // }
     }
 
     removeNomination = (title) => {
@@ -128,12 +65,6 @@ export class App extends Component {
 		this.setState({nominations});
 		//remove from localstorage
 		localStorage['nominations'] = JSON.stringify(nominations);
-
-        //reset currentMovie in state
-        // this.setState({currentMovie: {...this.state.currentMovie, title: ''}})
-        // this.setState({currentMovie: {...this.state.currentMovie, releaseYear: null}})
-        // this.setState({currentMovie: {...this.state.currentMovie, plot: ''}})
-		// this.setState({currentMovie: {...this.state.currentMovie, imdbID: null}})
 	}
 
     componentDidMount() {
@@ -148,10 +79,7 @@ export class App extends Component {
     render() {
         return (
             <div>
-                <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} searchResults={this.state.searchResults} nominate={this.nominate} />
-                
-                {/* {!!this.state.currentMovie ? null : <MovieShowPage currentMovie={this.state.currentMovie} nominate={this.nominate} nominations={this.state.nominations} />}  */}
-                
+                <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} searchResults={this.state.searchResults} nominate={this.nominate} nominations={this.state.nominations} />                
                 {this.state.nominations.length === 0 ? null : <Nominations nominations={this.state.nominations} removeNomination={this.removeNomination} />}
             </div>
         )
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,31 +2,6 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 
 export class Search extends Component {
-    // state = {
-    //     searchResults: [],
-    //     searchTerm: null
-    // }
-
-    // handleSearch = (searchTerm) => {
-    //     let search = searchTerm.target.value;
-    //     this.setState({searchTerm: search});
-    //     // this.props.handleSearch(searchTerm);
-    //     console.log(search);
-    //     let searchResults = this.state.searchResults;
-    
-    //     //axios request
-    //     axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&t=${search}`)
-    //     .then(res => {
-    //         if(res.data['Response'] !== "False")
-    //         {
-    //             searchResults.push(res.data['Title']); //push the whole movie obj that is returned into the array
-    //             console.log(res.data);
-    //             let uniqueResults = [...new Set(searchResults)]; //make it a set to remove duplicates
-    //             this.setState({uniqueResults});
-    //         }
-    //     })
-    // }
-
     render() {
         return (
             <div>
@@ -41,10 +16,10 @@ export class Search extends Component {
                     </form>
                 </div>
                 <div>
-                    <h2>Results:</h2>
+                    <h2>{`Results for: ${this.props.searchTerm}`}</h2>
                     <ul>
                         {this.props.searchResults.map((movie, index) => (
-                            <li key={index}>{movie} <button onClick={this.props.nominate}>Nominate</button></li>
+                            <li key={index}>{movie} <button onClick={this.props.nominate} disabled={this.props.nominations.includes(movie)}>Nominate</button></li>
                         ))}
                     </ul>
                 </div>
@@ -54,37 +29,3 @@ export class Search extends Component {
 }
 
 export default Search
-
-
-
-// import React from 'react'
-
-// export default function Search(props) {
-//     let searchInput = React.createRef();
-
-//     function handleClick(event) {
-//         event.preventDefault();
-//         props.handleSearch(searchInput.current.value)
-//     }
-
-//     return (
-//         <div>
-//             <h1>Search</h1>
-//             <p>This is the search page</p>
-//             <form>
-//                 <label>
-//                     Search Movie: <input type='text' name='movie' onChange={props.handleSearch} ref={searchInput} />
-//                     <button onClick={handleClick}>Search</button>
-//                 </label>
-//             </form>
-//             <div>
-//                 <h2>Results:</h2>
-//                 <ul>
-//                     {props.searchResults.map((movie, index) => (
-//                         <li key={index}>{movie['Title']}</li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         </div>
-//     )
-// }
